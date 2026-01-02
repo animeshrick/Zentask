ee0e975760b5e29004640414c19dfe504ecb0c4f
ANIMESH BANERJEE <43946793+animeshrick@users.noreply.github.com>
2026-01-03 01:42:00 +0530

    Auth (#5)
    * Installed required packages
    
    * auth docs
    
    * running base auth perfectly
    
    * adding log file service
    
    * deleted

---
+++ backend/Auth/routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const log = require("./services/log_service/logger");
+
+router.get("/v1", (req, res) => {
+    log("Server starting", {data: "maww"}, "mauu1212");
+    res.json({ message: "Auth API V1" });
+});
+
+module.exports = router;
