7b0e2434e531e69daa3008f6660301cee3f1579e
Animesh <animeshece1998@gmail.com>
2026-01-12 22:56:54 +0530

    update

--- backend/pure_authentication/pure_authentication/settings.py
+++ backend/pure_authentication/pure_authentication/settings.py
@@ -54,21 +54,21 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "pure_authentication.urls"
 
-# TEMPLATES = [
-#     {
-#         "BACKEND": "django.template.backends.django.DjangoTemplates",
-#         "DIRS": [BASE_DIR / "pure_authentication" / "templates"],
-#         "APP_DIRS": True,
-#         "OPTIONS": {
-#             "context_processors": [
-#                 "django.template.context_processors.debug",
-#                 "django.template.context_processors.request",
-#                 "django.contrib.auth.context_processors.auth",
-#                 "django.contrib.messages.context_processors.messages",
-#             ],
-#         },
-#     },
-# ]
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "pure_authentication" / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
 
 WSGI_APPLICATION = "pure_authentication.wsgi.application"
 
